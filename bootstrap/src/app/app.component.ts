2dc325b26277d3f6262946782aa00bd19eeb5e92
ethangonzales <ethangonzales@example.com>
2024-12-06 22:00:34 +0800

    first commit

---
+++ bootstrap/src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { FeaturedSectionComponent } from './featured-section/featured-section.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { FooterComponent } from './footer/footer.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, HeaderComponent, FeaturedSectionComponent, GalleryComponent, FooterComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'bootstrap';
+}
